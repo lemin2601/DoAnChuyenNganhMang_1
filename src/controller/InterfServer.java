80cc38ef0ee89d101d8a4d94650153e41127f81a
Uy Tran <vanuy113@gmail.com>
2017-11-25 18:16:41 +0400

    gui client

--- src/controller/InterfServer.java
+++ src/controller/InterfServer.java
@@ -12,7 +12,15 @@ import java.rmi.RemoteException;
 public interface InterfServer extends Remote {
 
     <T> T execute(Task<T> t) throws RemoteException;
+
+    /**
+     *
+     * @param ip
+     * @return
+     * @throws RemoteException
+     */
     String getIP() throws RemoteException;
+    void setIP(String ip) throws RemoteException;
     void CheckConnection() throws  RemoteException;
     boolean AddServer(InterfServer server) throws  RemoteException;
     boolean RemoveServer(InterfServer server) throws  RemoteException;
