74ecfdae027bdda1e79cb0301056718a5b95d56b
Le Minh Trung <lemin2601@gmail.com>
2017-11-25 15:35:17 +0700

    first commit

--- src/conf/Configure.java
+++ src/conf/Configure.java
@@ -4,6 +4,6 @@ package conf;
  * Created by Administrator on 10/21/2017.
  */
 public class Configure {
-    public static int PORT = 8888;
+    public static int PORT = 8978;
     public static String DATABASE_SERVICE_NAME ="ABCASC";
 }
